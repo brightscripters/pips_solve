2b54732e99e147e255f3a886a91711006a18ab66
Udi <udi@brightscripters.com>
2025-10-27 21:11:38 -0400

    rotation with test

--- pips.py
+++ pips.py
@@ -2,22 +2,32 @@
 def newSquare():
     return {''}
 
-# Piece class
+# Domino Piece class
 class piece:
-    def __init__(self, numbers):
-        self.numbers = numbers # Array [0,1]
-        self.rotated = False
+    def __init__(self, a, b ):
+        self.a = a
+        self.b = b
 
-    def swap(self):
-        firstNumber = self.numbers[0]
-        self.numbers[0] = self.numbers[1]
-        self.numbers[1] = firstNumber
+    # Rotation: 0, 1, 2, 3
+    # Retruns rotated piece
+    def rotate(self, rotation):
+
+        if not rotation in range(0,4):
+            print("ERROR: Rotation must be in range 0 to 3")
+            exit()
+
+        match rotation:
+            case 0:
+                return [[self.a, self.b],[None, None]]
+
+            case 1:
+                return [[self.a, None],[self.b, None]]
+
+            case 2:
+                return [[self.b, self.a],[None, None]]
+
+            case 3:
+                return [[self.b, None],[self.a, None]]
 
 
-    # Toggle rotation state
-    def rotate(self):
-        if self.rotated:
-            self.rotated = False
-        else:
-            self.rotated = True
         
--- test.py
+++ test.py
@@ -1,25 +1,40 @@
-# Test rules functionss
+# Test rules functions
 
 import rules
-import inspect
+import pips
+import pprint
 
-def newTest( arg, expect, msg ):
+
+def newTestItem( arg, expect, msg = 'Unexpected' ):
     return { 'arg': arg, 'expect': expect, 'msg': msg}
 
 
 def testThis( funcTest ):
     for test in funcTest['tests']:
         if funcTest['fn']( test['arg'] ) != test['expect']:
-            print('*** FAILED: ', test['msg'], 'in', funcTest['fnName'])
+            print('*** FAILED: ', test['msg'], 'in', funcTest['fnName'] + '(',test['arg'],')')
             exit()
+
+
+########################################################## piece.rotate
+oneTwo = pips.piece(1,2)
+funcTest = { 'fn': oneTwo.rotate, 'tests': [], 'fnName':'pips.piece().rotate' }
+tests = funcTest['tests']
+
+tests.append( newTestItem( 0, [[1, 2], [None, None]] ) )
+tests.append( newTestItem( 1, [[1, None], [2, None]] ) )
+tests.append( newTestItem( 2, [[2, 1], [None, None]] ) )
+tests.append( newTestItem( 3, [[2, None], [1, None]] ) )
+
+testThis( funcTest )
 
 ########################################################## allSame
 funcTest = { 'fn': rules.allSame, 'tests': [], 'fnName':'allSame()' }
 tests = funcTest['tests']
-tests.append( newTest( [],    None,   'MUST FAIL on empty list' ) )
-tests.append( newTest( [1],   None,   'MUST FAIL on single item list' ) )
-tests.append( newTest( [1,2], False,  'Should return False.' ) )
-tests.append( newTest( [1,1], True,   'Should return True.' ) )
+tests.append( newTestItem( [],    None,   'MUST FAIL on empty list' ) )
+tests.append( newTestItem( [1],   None,   'MUST FAIL on single item list' ) )
+tests.append( newTestItem( [1,2], False,  'Should return False.' ) )
+tests.append( newTestItem( [1,1], True,   'Should return True.' ) )
 
 testThis( funcTest )
 
@@ -27,37 +42,37 @@ testThis( funcTest )
 ########################################################## allDifferent
 funcTest = { 'fn': rules.allDifferent, 'tests': [], 'fnName':'allDifferent()' }
 tests = funcTest['tests']
-tests.append( newTest( [],      None,   'MUST FAIL on empty list' ) )
-tests.append( newTest( [1],     None,   'MUST FAIL on single item list' ) )
-tests.append( newTest( [1,2],   True,   'Should return True.' ) )
-tests.append( newTest( [1,2,3], True,   'Should return True.' ) )
-tests.append( newTest( [1,2,1], False,  'Should return False.' ) )
+tests.append( newTestItem( [],      None,   'MUST FAIL on empty list' ) )
+tests.append( newTestItem( [1],     None,   'MUST FAIL on single item list' ) )
+tests.append( newTestItem( [1,2],   True,   'Should return True.' ) )
+tests.append( newTestItem( [1,2,3], True,   'Should return True.' ) )
+tests.append( newTestItem( [1,2,1], False,  'Should return False.' ) )
 
 testThis( funcTest )
 
 ########################################################## makeSumIs
 funcTest = { 'fn': rules.makeSumIs(6), 'tests': [], 'fnName':'makeSumIs()' }
 tests = funcTest['tests']
-tests.append( newTest( [1,2,3],   True,  'Expecting True for total of 6' ) )
-tests.append( newTest( [1,2,3,4], False, 'Expecting False for total not 6' ) )
+tests.append( newTestItem( [1,2,3],   True,  'Expecting True for total of 6' ) )
+tests.append( newTestItem( [1,2,3,4], False, 'Expecting False for total not 6' ) )
 
 testThis( funcTest )
 
 ########################################################## makeSumLt
 funcTest = { 'fn': rules.makeSumLt(6), 'tests': [], 'fnName':'makeSumLt()' }
 tests = funcTest['tests']
-tests.append( newTest( [1,2,2],   True,  'Expecting True for total less than 6' ) )
-tests.append( newTest( [1,2,3],   False, 'Expecting False for total == 6' ) )
-tests.append( newTest( [1,2,4],   False, 'Expecting False for total > 6' ) )
+tests.append( newTestItem( [1,2,2],   True,  'Expecting True for total less than 6' ) )
+tests.append( newTestItem( [1,2,3],   False, 'Expecting False for total == 6' ) )
+tests.append( newTestItem( [1,2,4],   False, 'Expecting False for total > 6' ) )
 
 testThis( funcTest )
 
 ########################################################## makeSumGt
 funcTest = { 'fn': rules.makeSumGt(6), 'tests': [], 'fnName':'makeSumGt()' }
 tests = funcTest['tests']
-tests.append( newTest( [1,2,4],   True,  'Expecting True for total > 6' ) )
-tests.append( newTest( [1,2,3],   False, 'Expecting False for total == 6' ) )
-tests.append( newTest( [1,2,2],   False, 'Expecting False for total < 6' ) )
+tests.append( newTestItem( [1,2,4],   True,  'Expecting True for total > 6' ) )
+tests.append( newTestItem( [1,2,3],   False, 'Expecting False for total == 6' ) )
+tests.append( newTestItem( [1,2,2],   False, 'Expecting False for total < 6' ) )
 
 testThis( funcTest )
 
