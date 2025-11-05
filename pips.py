3ca972ed1ef1424d959be5852a89512eee4aed15
Udi <udi@brightscripters.com>
2025-11-04 23:20:41 -0500

    rotationsHere(), isSquareAvailable()

--- pips.py
+++ pips.py
@@ -1,12 +1,25 @@
+import copy
+
 # New square
 def newSquare():
     return {''}
 
 # Domino Piece class
 class piece:
-    def __init__(self, a, b ):
-        self.a = a
-        self.b = b
+    def __init__(self, a, b, bOffsets = [1,0] ):
+        self.nums = [a,b]
+        self.bOffsets = bOffsets # Rotation: b points right, down, left, up
+        self.reversed = False
+
+    def getRotOffsets(self):
+        return self.bOffsets
+
+    # swapps self. a <-> b
+    def reverse(self):
+        clone = copy.deepcopy(self)
+        clone.nums = [ clone.nums[1], clone.nums[0] ]
+        clone.reversed = True
+        return clone
 
     # Rotation: 0, 1, 2, 3
     # Retruns rotated piece
@@ -15,19 +28,61 @@ class piece:
         if not rotation in range(0,4):
             print("ERROR: Rotation must be in range 0 to 3")
             exit()
+        
+        clone = copy.deepcopy(self)
 
         match rotation:
-            case 0:
-                return [[self.a, self.b],[None, None]]
+            case 0: # defalt. No rotation. Pointing right.
+                pass
 
-            case 1:
-                return [[self.a, None],[self.b, None]]
+            case 1: # down
+                clone.bOffsets = [0,1]
 
-            case 2:
-                return [[self.b, self.a],[None, None]]
+            case 2: # left
+                clone.bOffsets = [-1,0]
 
-            case 3:
-                return [[self.b, None],[self.a, None]]
+            case 3: # up
+                clone.bOffsets = [0,-1]
+
+        return clone
+
+# find available piece rotations at coordinate
+# returns a list of rotations
+def rotationsHere(coordinate, board = [[-1,-1]]):
+    x,y = coordinate
+    if x < 0 or y < 0:
+        breakpoint()
+
+    rotations = []
+
+    if isSquareAvailable(x+1, y, board):
+        rotations.append(0)
+
+    if isSquareAvailable(x, y+1, board):
+        rotations.append(1)
+
+    if isSquareAvailable(x-1, y, board):
+        rotations.append(2)
+
+    if isSquareAvailable(x, y-1, board):
+        rotations.append(3)
+
+    return rotations
 
 
-        
+def isSquareAvailable(x,y,board):
+    
+    # x out or range
+    if x >= len(board[0]) or x < 0:
+        return False
+
+    # y out or range
+    if y >= len(board) or y < 0:
+        return False
+
+    if board[y][x] == -1:
+        return True
+
+    return False
+
+    
--- test.py
+++ test.py
@@ -11,20 +11,53 @@ def newTestItem( arg, expect, msg = 'Unexpected' ):
 
 def testThis( funcTest ):
     for test in funcTest['tests']:
-        if funcTest['fn']( test['arg'] ) != test['expect']:
+        
+        if test['arg'] == None:
+            result = funcTest['fn']()
+        else:
+            result = funcTest['fn']( test['arg'] )
+
+        if  result != test['expect']:
             print('*** FAILED: ', test['msg'], 'in', funcTest['fnName'] + '(',test['arg'],')')
             exit()
 
 
-########################################################## piece.rotate
 oneTwo = pips.piece(1,2)
-funcTest = { 'fn': oneTwo.rotate, 'tests': [], 'fnName':'pips.piece().rotate' }
+board = [[-1]] * 10
+
+########################################################## pips.rotationsHere()
+testFn = lambda coordinate: pips.rotationsHere(coordinate, board)
+funcTest = { 'fn': testFn, 'tests': [], 'fnName':'rotationsHere()' }
 tests = funcTest['tests']
 
-tests.append( newTestItem( 0, [[1, 2], [None, None]] ) )
-tests.append( newTestItem( 1, [[1, None], [2, None]] ) )
-tests.append( newTestItem( 2, [[2, 1], [None, None]] ) )
-tests.append( newTestItem( 3, [[2, None], [1, None]] ) )
+tests.append( newTestItem( (0,0), [1] ) )
+tests.append( newTestItem( (0,1), [1,3] ) )
+tests.append( newTestItem( (0,8), [1,3] ) )
+tests.append( newTestItem( (0,9), [3] ) )
+
+testThis( funcTest )
+exit()
+
+########################################################## piece.rotate
+testFn = lambda rotation: oneTwo.rotate(rotation).getRotOffsets()
+funcTest = { 'fn': testFn, 'tests': [], 'fnName':'pips.piece().rotate' }
+tests = funcTest['tests']
+
+tests.append( newTestItem( 0, [  1, 0]  ) )
+tests.append( newTestItem( 1, [  0, 1]  ) )
+tests.append( newTestItem( 2, [ -1, 0] ) )
+tests.append( newTestItem( 3, [  0,-1] ) )
+
+testThis( funcTest )
+
+
+########################################################## piece.reverse()
+testFn = lambda : oneTwo.reverse().nums
+funcTest = { 'fn': testFn, 'tests': [], 'fnName':'pips.piece().reverse()' }
+tests = funcTest['tests']
+
+tests.append( newTestItem( None, pips.piece(2,1).nums  ) )
+
 
 testThis( funcTest )
 
@@ -76,4 +109,4 @@ tests.append( newTestItem( [1,2,2],   False, 'Expecting False for total < 6' ) )
 
 testThis( funcTest )
 
-print("PASSED")
+print("PASSED\n")
